94f2e4d335dcbe9b5ff3fa3518e46c9769b27ed2
Eric Pierce <wapcaplet88@gmail.com>
2011-12-08 11:10:00 -0700

    Documentation

--- lib/jiraby/jira.rb
+++ lib/jiraby/jira.rb
@@ -9,6 +9,47 @@ require 'rest_client'
 # Local
 require 'jiraby/issue'
 
+# JIRA REST API Methods
+# (Full list: http://docs.atlassian.com/jira/REST/latest/)
+#
+# /auth/1/session
+# /auth/1/websudo
+# /api/2.0.alpha1/issueLink
+# /api/2.0.alpha1/issue/{issueKey}
+# /api/2.0.alpha1/issue/{issueKey}/transitions
+# /api/2.0.alpha1/issue/{issueKey}/votes
+# /api/2.0.alpha1/issue/{issueKey}/watchers
+# /api/2.0.alpha1/groups/picker?query
+# /api/2.0.alpha1/version
+# /api/2.0.alpha1/version/{id}?moveFixIssuesTo&moveAffectedIssuesTo
+# /api/2.0.alpha1/version/{id}/relatedIssueCounts
+# /api/2.0.alpha1/version/{id}/unresolvedIssueCount
+# /api/2.0.alpha1/version/{id}/move
+# /api/2.0.alpha1/comment/{id}?render
+# /api/2.0.alpha1/project/{projectKey}/role
+# /api/2.0.alpha1/project/{projectKey}/role/{id}
+# /api/2.0.alpha1/user?username
+# /api/2.0.alpha1/serverInfo
+# /api/2.0.alpha1/component
+# /api/2.0.alpha1/component/{id}?moveIssuesTo
+# /api/2.0.alpha1/component/{id}/relatedIssueCounts
+# /api/2.0.alpha1/search?jql&startAt&maxResults
+# /api/2.0.alpha1/project
+# /api/2.0.alpha1/project/{key}
+# /api/2.0.alpha1/project/{key}/versions
+# /api/2.0.alpha1/project/{key}/components
+# /api/2.0.alpha1/status/{id}
+# /api/2.0.alpha1/issueLinkType
+# /api/2.0.alpha1/issueLinkType/{issueLinkTypeId}
+# /api/2.0.alpha1/customFieldOption/{id}
+# /api/2.0.alpha1/resolution/{id}
+# /api/2.0.alpha1/issueType/{id}
+# /api/2.0.alpha1/attachment/{id}
+# /api/2.0.alpha1/priority/{id}
+# /api/2.0.alpha1/application-properties?key&value
+# /api/2.0.alpha1/worklog/{id}
+# /api/2.0.alpha1/issue/{issueKey}/attachments
+
 module Jiraby
 
   class Jira
@@ -21,19 +62,35 @@ module Jiraby
     end
 
 
-    # Return the URL for authenticating to Jira
+    # Return the URL for authenticating to Jira.
     def auth_url
       "#{@url}/rest/auth/1/session"
     end
 
 
-    # Return the URL for the Jira REST API path, followed by the given subpath
+    # Return the full URL for the given REST API subpath.
+    #
+    # @param [String] subpath
+    #   The last part of the REST API path
+    #
+    # @return [String]
+    #   The full URL for the given REST API subpath
+    #
     def rest_url(subpath)
       "#{@url}/rest/api/2.0.alpha1/#{subpath}"
     end
 
 
-    # Login to Jira using the given username/password
+    # Login to Jira using the given username/password.
+    #
+    # @param [String] username
+    #   Log in as this user
+    # @param [String] password
+    #   Password for the given username
+    #
+    # @return [Bool]
+    #   `true` if login was successful, `false` otherwise
+    #
     def login(username, password)
       request_json = Yajl::Encoder.encode({
         :username => username,
@@ -60,6 +117,11 @@ module Jiraby
     end
 
 
+    # Return the headers needed for most REST requests, including
+    # the current session data.
+    #
+    # @return [Hash]
+    #
     def headers
       {
         :content_type => :json,
@@ -69,31 +131,62 @@ module Jiraby
     end
 
 
-    def search(jql, start_at, max_results)
+    # Invoke the 'search' method to find issues matching the given JQL query,
+    # and return the raw JSON response.
+    #
+    # @param [String] jql
+    #   JQL query for the issues you want to match
+    # @param [Integer, String] start_at
+    #   0-based index of the first issue to match
+    # @param [Integer, String] max_results
+    #   Maximum number of issues to return
+    #
+    def search(jql, start_at=0, max_results=50)
       return post(
         'search',
         {
           :jql => jql,
-          :startAt => start_at,
-          :maxResults => max_results,
+          :startAt => start_at.to_i,
+          :maxResults => max_results.to_i,
         }
       )
     end
 
 
     # Return the Issue with the given key.
+    #
+    # @param [String] key
+    #   The issue's unique identifier (usually like PROJ-NNN)
+    #
+    # @return [Issue]
+    #   An Issue populated with data returned by the API
+    #
     def issue(key)
       return Jiraby::Issue.new(get("issue/#{key}"))
     end
 
 
     # Return the total number of issues matching the given JQL query.
+    #
+    # @param [String] jql
+    #   JQL query for the issues you want to match
+    #
+    # @return [Integer]
+    #   Number of issues matching the query
+    #
     def count(jql='')
-      search(jql, 0, 1)['total']
+      return search(jql, 0, 1)['total']
     end
 
 
     # Return all of the issue keys matching the given JQL query.
+    #
+    # @param [String] jql
+    #   JQL query for the issues you want to match
+    #
+    # @return [Array<String>]
+    #   The keys of all issues matching the query
+    #
     def issue_keys(jql='')
       # Issue keys will be accumulated here
       keys = []
@@ -115,7 +208,15 @@ module Jiraby
     end
 
 
-    # Yield all of the Issues matching the given JQL query.
+    # Find all issues matching the given JQL query, and return a Generator
+    # that yields each one as an Issue object. Each Issue is fetched from
+    # the REST API as needed.
+    #
+    # @param [String] jql
+    #   JQL query for the issues you want to match
+    #
+    # @return [Generator]
+    #
     def issues(jql='')
       keys = issue_keys(jql)
       issue_generator = Generator.new do |g|
@@ -130,6 +231,16 @@ module Jiraby
     # Submit a POST request to the given REST subpath, including
     # the given JSON parameters. If the request succeeds, return
     # a JSON-formatted response. Otherwise, return nil.
+    #
+    # @param [String] subpath
+    #   The last part of the REST API path you want to post to
+    # @param [Hash] params
+    #   Key => value parameters to post
+    #
+    # @return [Hash]
+    #   Raw JSON response converted to a Ruby Hash, or nil
+    #   if the request failed.
+    #
     def post(subpath, params={})
       json = Yajl::Encoder.encode(params)
       begin
@@ -144,6 +255,14 @@ module Jiraby
 
     # Submit a GET request to the given REST subpath. If the request succeeds,
     # return a JSON-formatted response. Otherwise, return nil.
+    #
+    # @param [String] subpath
+    #   The last part of the REST API path you want to post to
+    #
+    # @return [Hash, nil]
+    #   Raw JSON response converted to a Ruby Hash, or nil
+    #   if the request failed.
+    #
     def get(subpath)
       begin
         response = RestClient.get(rest_url(subpath), headers)
@@ -154,46 +273,5 @@ module Jiraby
       end
     end
 
-    # Methods:
-    # Full list: http://docs.atlassian.com/jira/REST/latest/
-    #
-    # /auth/1/session
-    # /auth/1/websudo
-    # /api/2.0.alpha1/issueLink
-    # /api/2.0.alpha1/issue/{issueKey}
-    # /api/2.0.alpha1/issue/{issueKey}/transitions
-    # /api/2.0.alpha1/issue/{issueKey}/votes
-    # /api/2.0.alpha1/issue/{issueKey}/watchers
-    # /api/2.0.alpha1/groups/picker?query
-    # /api/2.0.alpha1/version
-    # /api/2.0.alpha1/version/{id}?moveFixIssuesTo&moveAffectedIssuesTo
-    # /api/2.0.alpha1/version/{id}/relatedIssueCounts
-    # /api/2.0.alpha1/version/{id}/unresolvedIssueCount
-    # /api/2.0.alpha1/version/{id}/move
-    # /api/2.0.alpha1/comment/{id}?render
-    # /api/2.0.alpha1/project/{projectKey}/role
-    # /api/2.0.alpha1/project/{projectKey}/role/{id}
-    # /api/2.0.alpha1/user?username
-    # /api/2.0.alpha1/serverInfo
-    # /api/2.0.alpha1/component
-    # /api/2.0.alpha1/component/{id}?moveIssuesTo
-    # /api/2.0.alpha1/component/{id}/relatedIssueCounts
-    # /api/2.0.alpha1/search?jql&startAt&maxResults
-    # /api/2.0.alpha1/project
-    # /api/2.0.alpha1/project/{key}
-    # /api/2.0.alpha1/project/{key}/versions
-    # /api/2.0.alpha1/project/{key}/components
-    # /api/2.0.alpha1/status/{id}
-    # /api/2.0.alpha1/issueLinkType
-    # /api/2.0.alpha1/issueLinkType/{issueLinkTypeId}
-    # /api/2.0.alpha1/customFieldOption/{id}
-    # /api/2.0.alpha1/resolution/{id}
-    # /api/2.0.alpha1/issueType/{id}
-    # /api/2.0.alpha1/attachment/{id}
-    # /api/2.0.alpha1/priority/{id}
-    # /api/2.0.alpha1/application-properties?key&value
-    # /api/2.0.alpha1/worklog/{id}
-    # /api/2.0.alpha1/issue/{issueKey}/attachments
-    #
   end
 end
