d1c2a8b73fb9b3400d2be08100a68dba401a78f3
Eric Pierce <wapcaplet88@gmail.com>
2012-01-13 10:01:28 -0700

    Specs for not_implemented_in

--- spec/jira_spec.rb
+++ spec/jira_spec.rb
@@ -32,5 +32,19 @@ describe Jiraby::Jira do
       jira.rest_url('issue').should == 'http://jira.example.com/rest/api/2/issue'
     end
   end
+
+  describe '#not_implemented_in' do
+    it "raises an exception when API version is one of those listed" do
+      jira = Jiraby::Jira.new('jira.example.com', '2.0.alpha1')
+      lambda do
+        jira.not_implemented_in('Issue creation', '2.0.alpha1')
+      end.should raise_error
+    end
+
+    it "returns nil when API version is not one of those listed" do
+      jira = Jiraby::Jira.new('jira.example.com', '2')
+      jira.not_implemented_in('Issue creation', '2.0.alpha1').should be_nil
+    end
+  end
 end
 
