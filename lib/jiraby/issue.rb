0f4dcc33e7f0baea0e32612deff994324036517d
Eric Pierce <wapcaplet88@gmail.com>
2014-01-14 11:15:33 -0700

    Allow Issue#key to pass-thru
    Also begin thinking about how to implement human-readable field names
    for Issues

--- lib/jiraby/issue.rb
+++ lib/jiraby/issue.rb
@@ -2,10 +2,23 @@ require 'hashie'
 
 module Jiraby
   class Issue < Hashie::Mash
-    # Work around Hash's builtin `key` method, since Jira issues use `key` for
-    # their ID.
-    def key
-      return self['key']
+    # Return the value in the `key` field (Jira's issue identifier).
+    def key(*args)
+      # Pass-through to Hash's `key` method
+      if args.length > 0
+        super(*args)
+      else
+        return self['key']
+      end
+    end
+
+    # Set the field named `name` equal to `value`.
+    # `name` may be the field identifier (like `subtasks` or `customfield_10001`),
+    # or it may be the human-readable field name (like `Sub-Tasks` or `My Custom Field`).
+    #
+    def set(name, value)
+      # TODO: In order to implement this, the Issue class will need access to
+      # Jira's field name mappings (`Jiraby::Jira#fields`)
     end
   end
 end
