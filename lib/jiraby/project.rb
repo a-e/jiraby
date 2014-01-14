5a099dbba094b5b71ec60fe97b318125a78e91e1
Eric Pierce <wapcaplet88@gmail.com>
2014-01-14 10:33:50 -0700

    Make Project < Hashie::Mash

--- lib/jiraby/project.rb
+++ lib/jiraby/project.rb
@@ -1,16 +1,14 @@
-module Jiraby
-  class Project
-    def initialize(json)
-      @json = json
-    end
+require 'hashie'
 
+module Jiraby
+  class Project < Hashie::Mash
     # Return a hash of issue types for the project, indexed by the name of the
     # issue type.
     #
     def issue_types
       # Index by issue type name
       result = {}
-      @json['issueTypes'].each do |issue_type|
+      self['issueTypes'].each do |issue_type|
         result[issue_type['name']] = issue_type
       end
       return result
